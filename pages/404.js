58d3b343be4a06ed94de889823a0efbae042098b
kosuke <ishtar1977@gmail.com>
2020-11-18 21:49:15 +0900

    third commit

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "../components/layout.module.css";
+import utilstyles from "../styles/utils.module.css";
+
+export default function Custom404() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>404 - Page Not Found</h1>
+      <div className={utilstyles.ftfimg}>
+        <img src="/404.gif" alt="404" />
+      </div>
+
+      <div className={styles.backToHome}>
+        <Link href="/">
+          <a>← Back to home</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- pages/tags.js
+++ pages/tags.js
@@ -14,7 +14,6 @@ export async function getStaticProps() {
 }
 
 export default function Tags({allTags}) {
-    console.log(allTags)
     return (
       <Layout>
         <Head>
