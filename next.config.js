6d6befdd3b5776ed25912d813a40de9d656f7591
Squigly77 <ishtar1977@gmail.com>
2021-06-16 21:46:23 +0900

    next.js update

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ module.exports = withCSS({
 module.exports = {
   webpack: (config) => {
     config.node = {
-      fs: "empty",
-      /*child_process: "empty",
+      /*fs: "empty",
+      child_process: "empty",
       net: "empty",
       dns: "empty",
       tls: "empty",*/
